31dc61c40a31f78933fd5043fe46098139362d5e
root <root@DESKTOP-SIH6VNN.localdomain>
2022-12-23 11:59:12 +0100

    footer css

--- octodiving/src/Footer/Footer.jsx
+++ octodiving/src/Footer/Footer.jsx
@@ -1,40 +1,53 @@
+
+import {Link} from 'react-router-dom';
 import "./stylesFooter.scss";
 
 function Footer (){
     return (
         <div className="footer">
-            <div>
-                <div>
-                    <h1>Nos clients</h1>
-                    <div>Future liste des clients</div>
-                </div>
-                <div>
+            <div className="contact">
+                <div className="contactContainer">
                     <div>
-                        <div>
-                            <img src="" alt="Telephone" />
-                        </div>
-                        <div>
-                            <p>Nous appeler: </p>
-                            <p>+33 6 30 24 27 74</p>
-                        </div>
-                        <div>
-                            <img src="" alt="E-mail" />
-                        </div>
-                        <div>
-                            <p>E-mail: </p>
-                            <a href="">florian.bessiere.pro@gmail.com</a>
+                        <h1></h1>   {/* Nos clients */}
+                        <div className="clientList"></div> {/* La liste des client, css non fait attente du format*/}
+                    </div>
+                    <div>
+                        <div className="contactDetail">
+                            <div className="way">
+                                <div>
+                                    <img src="../img/phone-call.svg" alt="Telephone" />
+                                </div>
+                                <div className="contactText">
+                                    <p className="contactTitle">Nous appeler: </p>
+                                    <p className="contactValue">+33 6 30 24 27 74</p>
+                                </div>
+                            </div>
+                            <div className="way">
+                                <div>
+                                    <img src="../img/mail.svg" alt="E-mail" />
+                                </div>
+                                <div className="contactText">
+                                    <p className="contactTitle">E-mail: </p>
+                                    <Link to="florian.bessiere.pro@gmail.com" className="contactValue">florian.bessiere.pro@gmail.com</Link>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div></div>
                 </div>
             </div>
-            <div>
-                <div>
-                    <a href="">Octo Diving</a>
+            <div className='endingFooter'>
+                <div className="returnPresentation">
+                    <Link to="/presentation">
+                        <img src="../img/nom-vectoriel-couleur.png" alt="Logo-site" />
+                    </Link>
                 </div>
-                <div>
-                    <a href="">logo Facebook</a>
-                    <a href="">logo Instagram</a>
+                <div className="logo">
+                    <Link to="https://www.facebook.com/profile.php?id=100087820029338" className='linkLogo'>
+                        <img src="../img/facebook.svg" alt="logo-facebook"/>
+                    </Link>
+                    <Link to="https://www.instagram.com/octo.diving.fr/" className='linkLogo'>
+                        <img src="../img/instagram.svg" alt="logo-instagram" />
+                    </Link>
                 </div>
             </div>
         </div>
