47572dcf8e6a9319cf214b55dd081d892a153af9
root <root@DESKTOP-SIH6VNN.localdomain>
2022-12-29 11:57:17 +0100

    formulaire en cours

--- octodiving/src/Formulaire/Formulaire.jsx
+++ octodiving/src/Formulaire/Formulaire.jsx
@@ -1,37 +1,39 @@
-import React, { Component } from 'react'
-import { Form } from 'semantic-ui-react'
+import { Checkbox, Form } from 'semantic-ui-react'
 
-const options = [
-  { key: 'm', text: 'Male', value: 'male' },
-  { key: 'f', text: 'Female', value: 'female' },
-  { key: 'o', text: 'Other', value: 'other' },
-]
+function Formulaire () {
+  
+  return (
+    <Form>
+      <Form.Group widths='equal'> 
+        <Form.Input fluid label='Nom' placeholder='Votre nom' />
+        <Form.Input fluid label='Prénom' placeholder='Votre prénom' />
+        <Form.Input fluid label='E-mail' placeholder='Votre E-mail' />
+        <Form.Input fluid label='N° Téléphone' placeholder='Votre numéro' />
+      </Form.Group>
+      <Form.Group widths='equal'>
+        <Form.Input fluid label='Votre société' placeholder='Nom de votre société' />
+        <Form.Input fluid label='N° SIRET' placeholder='Votre n° SIRET' />
+      </Form.Group>
+      <Form.Group widths='equal'>
+        <Form.Input fluid label='Adresse de prestation' placeholder='Adresse' />
+        <Form.Input fluid label="Complément d'adresse" placeholder='Complément' />
+      </Form.Group>
+      <Form.Group widths='equal'>
+        <Form.Input fluid label='Numéro' placeholder='Numero' />
+        <Form.Input fluid label='Rue' placeholder='Rue' />
+        <Form.Input fluid label='Code postal' placeholder='Code postal' />
+        <Form.Input fluid label='Ville' placeholder='Ville' />
+      </Form.Group>
+      <Form.Field control={Checkbox} label={{ children: "L'adresse de facturation est la même que l'adresse de prestation" }} />
 
-class FormExampleSubcomponentControl extends Component {
-  state = {}
 
-  handleChange = (e, { value }) => this.setState({ value })
+      
+      <Form.TextArea label='Vous avez des questions ? ' placeholder='Tell us more about you...' />
+      <Form.Button>Submit</Form.Button>
+    </Form>
+  )
 
-  render() {
-    const { value } = this.state
-    return (
-      <Form>
-        <Form.Group widths='equal'>
-          <Form.Input fluid label='Votre société' placeholder='Nom de votre société' />
-        </Form.Group>
-        <Form.Group inline> 
-          <Form.Input fluid label='Nom' placeholder='Votre nom' />
-          <Form.Input fluid label='Prénom' placeholder='Votre prénom' />
-          <Form.Input fluid label='N° SIRET' placeholder='Votre n° SIRET' />
-          <Form.Input fluid label='E-mail' placeholder='Votre E-mail' />
-          <Form.Input fluid label='N° Téléphone' placeholder='Votre numéro' />          
-        </Form.Group>
-        
-        <Form.TextArea label='About' placeholder='Tell us more about you...' />
-        <Form.Button>Submit</Form.Button>
-      </Form>
-    )
-  }
 }
 
-export default FormExampleSubcomponentControl
+
+export default Formulaire;
