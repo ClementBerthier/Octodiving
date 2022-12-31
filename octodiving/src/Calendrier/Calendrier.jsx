c5833222f61102e59cd793dcf5d9abcf39bdf7b0
root <root@DESKTOP-SIH6VNN.localdomain>
2022-12-31 14:42:34 +0100

    calendrier ok

--- octodiving/src/Calendrier/Calendrier.jsx
+++ octodiving/src/Calendrier/Calendrier.jsx
@@ -6,11 +6,12 @@ function Calendrier (){
     return (
         <div className="calendrier">
             <NavBar />
-            <div>
-                <h1>Calendrier</h1>
-                <p>Calendrier des disponibilitées</p>
+            <div className="calendrierContainer">
                 <div>
-                    Calendrier
+                    <h1>Calendrier des disponibilitées</h1>
+                </div>
+                <div>
+                    <iframe src="https://calendar.google.com/calendar/embed?src=256b8ad70ad3e214fb07658fc5e691b5e8bf1eca8dce5e21e82e632414c6ea40%40group.calendar.google.com&ctz=Europe%2FParis" title="Calendrier des disponibilitées"width="800" height="600"></iframe>                    
                 </div>
             </div>
             <Footer />
