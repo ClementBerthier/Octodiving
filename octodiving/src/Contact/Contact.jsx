bb1bc3520a7d02a4dadb9457f1ae412480b683c3
root <root@DESKTOP-SIH6VNN.localdomain>
2022-12-31 13:35:16 +0100

    contact ok

--- octodiving/src/Contact/Contact.jsx
+++ octodiving/src/Contact/Contact.jsx
@@ -6,39 +6,43 @@ function Contact (){
     return (
         <div className="contact">
             <NavBar />
-            <div>
-                <div>
+            <div className="contactContainer">
+                <div className="contactTitle">
                     <h1>Nous Contacter</h1>
                 </div>
-                <div>
+                <div className="contactParagraph">
                     <h2>Parlons un peu</h2>
                     <p>Faites-nous part de vos réflexions.</p>
                 </div>
-                <div>
-                    <div>
-                        <div>
-                            <img src="" alt="Télephone" />
-                            <h3>NUMERO DE TELEPHONE</h3>
-                            <p>06 30 24 27 77</p>
+                <div className="infoContainer">
+                    <div className="logoContainer">
+                        <div className="genericContact">
+                            <div className="contactItem">
+                                <img src="./img/phone-call-contact.svg" alt="Télephone" />
+                                <h3>NUMERO DE TELEPHONE</h3>
+                                <p>06 30 24 27 77</p>
+                            </div>
+                            <div className="contactItem">
+                                <img src="./img/facebook-contact.svg" alt="Facebook" />
+                                <h3>FACEBOOK</h3>
+                                <a href="https://www.facebook.com/profile.php?id=100087820029338">Octo Diving</a>                                
+                            </div>
                         </div>
-                        <div>
-                            <img src="" alt="Email" />
-                            <h3>EMAIL</h3>
-                            <p>florian.bessiere.pro@gmail.com</p>
-                        </div>
-                        <div>
-                            <img src="" alt="Facebook" />
-                            <h3>FACEBOOK</h3>
-                            <a href="">Octo Diving</a>
-                        </div>
-                        <div>
-                            <img src="" alt="Instagram" />
-                            <h3>INSTAGRAM</h3>
-                            <a href="">Octo Diving</a>
+                        <div className="socialContact">
+                            <div className="contactItem">
+                                <img src="./img/mail-contact.svg" alt="Email" />
+                                <h3>EMAIL</h3>
+                                <p>florian.bessiere.pro@gmail.com</p>
+                            </div>
+                            <div className="contactItem instagram">
+                                <img src="./img/instagram-contact.svg" alt="Instagram" />
+                                <h3>INSTAGRAM</h3>
+                                <a href="https://www.instagram.com/octo.diving.fr/">Octo Diving</a>
+                            </div>
                         </div>
                     </div>
-                    <div>
-                        <img src="" alt="Plongeur" />
+                    <div className="imgContainer">
+                        <img src="./img/plongeur-contact.jpg" alt="Plongeur" />
                     </div>
 
                 </div>
