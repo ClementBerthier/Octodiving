7d4758b85e7c6237a91366d153eada9f94c5d171
root <root@DESKTOP-SIH6VNN.localdomain>
2023-01-03 09:55:57 +0100

    remerciement

--- octodiving/src/ConfirmationSendEmail/ConfirmationSendEmail.jsx
+++ octodiving/src/ConfirmationSendEmail/ConfirmationSendEmail.jsx
@@ -1,9 +1,16 @@
-
+import Footer from "../Footer/Footer";
+import NavBar from "../NavBar/NavBar";
+ 
 import "./stylesConfirmationSendEmail.scss";
 
 function ConfirmationSendEmail (){
     return (
-        <h1>merci</h1>
+        <div className="confirmationSendEmail">
+            <NavBar />
+            <h1>Merci! Je vous recontacte le plus rapidement possible</h1>            
+            <Footer />
+
+        </div>
     )
 }
 
